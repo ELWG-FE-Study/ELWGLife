a22fe698b4e1a6bcc790df17171c5353a3850fe9
lidasong <1584809032@qq.com>
2014-03-25 14:13:32 +0800

    models

--- models/contact_model.php
+++ models/contact_model.php
@@ -1,32 +1,45 @@
 <?php
-class contact_model
-{
-	function _construct(){
-
-	}
-
-	function get_contact_data（）{
-		$con = mysql_connect("115.156.216.95", "lidasong", "123");
-if (!$con) {
-    die('Could not connect: ' . mysql_error());
-}
-$db_selected = mysql_select_db('db_elwg_life', $con);
-if (!$db_selected) {
-    die('Could not connect: ' . mysql_error());
-}
-$result = mysql_query("SELECT * FROM contacts ");
-if (!$result) {
-	echo "no contacts information here in contacts";
-    $message = 'Invalid query: ' . mysql_error() . "\n";
-    die($message);
-}
-while ($row = mysql_fetch_array($result)) {
-	echo $row['member_name']." ".$row['sex']." ".$row['grade'].
-	" ".$row['phone']." ".$row['qq']." ".$row['email'];
-}
-mysql_close($con);
-}
-
-
-}
-?>
+class contact_model {
+    function _construct() {
+    }
+    function get_contact_data（） {
+        $con = mysql_connect("115.156.216.95", "lidasong", "123");
+        if (!$con) {
+            die('Could not connect: ' . mysql_error());
+        }
+        $db_selected = mysql_select_db('db_elwg_life', $con);
+        if (!$db_selected) {
+            die('Could not connect: ' . mysql_error());
+        }
+        $result = mysql_query("SELECT * FROM contacts ");
+        if (!$result) {
+            echo "no contacts information here in contacts";
+            $message = 'Invalid query: ' . mysql_error() . "\n";
+            die($message);
+        }
+        while ($row = mysql_fetch_array($result)) {
+            echo $row['member_name'] . " " . $row['sex'] . " " . $row['grade'] . " " . $row['phone'] . " " . $row['qq'] . " " . $row['email'];
+        }
+        mysql_close($con);
+    }
+    function update_contact_data($name, $sex, $grade, $tel, $qq, $email) {
+        $con = mysql_connect("115.156.216.95", "lidasong", "123");
+        if (!$con) {
+            die('Could not connect: ' . mysql_error());
+        }
+        $db_selected = mysql_select_db('db_elwg_life', $con);
+        if (!$db_selected) {
+            die('Could not connect: ' . mysql_error());
+        }
+        $search = mysql_query("SELECT member_name FROM contacts");
+        if ($search == $name) {
+            $result = mysql_query(" UPDATE contacts SET sex='$sex',grade='$grade',phone='$tel',
+		qq='$qq', email='$email' ");
+        } else $result = mysql_query("INSERT INTO contacts (member_name,sex,grade,phone,qq,email)
+ VALUES ('$name','$sex','$grade','$tel','$qq','$email')") if (!$result) {
+            echo "no contacts information here in contacts";
+            $message = 'Invalid query: ' . mysql_error() . "\n";
+            die($message);
+        }
+    }
+?>            
