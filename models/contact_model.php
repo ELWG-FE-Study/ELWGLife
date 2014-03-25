2edaaf0d8fe7c305ea6c59d43eb13ae6ce8712fd
lidasong <1584809032@qq.com>
2014-03-25 15:49:57 +0800

    models

--- models/contact_model.php
+++ models/contact_model.php
@@ -6,28 +6,15 @@ class Contact_model {
 
     }
     function get_contact_data（） {
-    	oprateSql(1);
-    }
-
-
-    function update_contact_data($name, $sex, $grade, $tel, $qq, $email) {
-	   	oprateSql(2);
-}
-
-$sql;
-function oprateSql($value){
-	switch (value) {
-		$con = mysql_connect("115.156.216.95", "lidasong", "123");
+        $con = mysql_connect("115.156.216.95", "lidasong", "123");
         if (!$con) {
             die('Could not connect: ' . mysql_error());
         }
         $db_selected = mysql_select_db('db_elwg_life', $con);
         if (!$db_selected) {
             die('Could not connect: ' . mysql_error());
-        }       
-		case 1:
-        $sql="SELECT * FROM contacts ";
-        $result = mysql_query($sql);
+        }
+        $result = mysql_query("SELECT * FROM contacts ");
         if (!$result) {
             echo "no contacts information here in contacts";
             $message = 'Invalid query: ' . mysql_error() . "\n";
@@ -36,10 +23,20 @@ function oprateSql($value){
         while ($row = mysql_fetch_array($result)) {
             echo $row['member_name'] . " " . $row['sex'] . " " . $row['grade'] . " " . $row['phone'] . " " . $row['qq'] . " " . $row['email'];
         }
-        break;
-        case 2:
-        $sql="SELECT member_name FROM contacts";
-        $search = mysql_query($sql);
+        mysql_close($con);
+    }
+
+
+    function update_contact_data($name, $sex, $grade, $tel, $qq, $email) {
+        $con = mysql_connect("115.156.216.95", "lidasong", "123");
+        if (!$con) {
+            die('Could not connect: ' . mysql_error());
+        }
+        $db_selected = mysql_select_db('db_elwg_life', $con);
+        if (!$db_selected) {
+            die('Could not connect: ' . mysql_error());
+        }
+        $search = mysql_query("SELECT member_name FROM contacts");
         if ($search == $name) {
             $result = mysql_query(" UPDATE contacts SET sex='$sex',grade='$grade',phone='$tel',
 		qq='$qq', email='$email' ");
@@ -50,12 +47,6 @@ function oprateSql($value){
             $message = 'Invalid query: ' . mysql_error() . "\n";
             die($message);
         }
-        break;
-        default:
-        break;
-	}
-	
-        mysql_close($con);
+    }
 }
-
 ?>            
