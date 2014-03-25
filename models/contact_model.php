0990efd9932c84063bc709a6f4f979be0678161a
lidasong <1584809032@qq.com>
2014-03-25 20:47:27 +0800

    model

--- models/contact_model.php
+++ models/contact_model.php
@@ -1,52 +1,56 @@
 <?php
 
 class Contact_model {
-    
     function _construct() {
-
-    }
-    function get_contact_data（） {
-        $con = mysql_connect("115.156.216.95", "lidasong", "123");
+        static $con = mysql_connect("115.156.216.95", "lidasong", "123");
         if (!$con) {
             die('Could not connect: ' . mysql_error());
         }
-        $db_selected = mysql_select_db('db_elwg_life', $con);
+        static $db_selected = mysql_select_db('db_elwg_life', $con);
         if (!$db_selected) {
             die('Could not connect: ' . mysql_error());
         }
-        $result = mysql_query("SELECT * FROM contacts ");
+    }
+    function get_contact_data（） {
+        $sql = "SELECT * FROM contacts ";
+        $result = mysql_query($sql);
+        $backArray[] = array();
         if (!$result) {
             echo "no contacts information here in contacts";
             $message = 'Invalid query: ' . mysql_error() . "\n";
             die($message);
         }
         while ($row = mysql_fetch_array($result)) {
-            echo $row['member_name'] . " " . $row['sex'] . " " . $row['grade'] . " " . $row['phone'] . " " . $row['qq'] . " " . $row['email'];
+            $backArray[] = $row;
         }
         mysql_close($con);
+        return $backArray;
     }
-
-
     function update_contact_data($name, $sex, $grade, $tel, $qq, $email) {
-        $con = mysql_connect("115.156.216.95", "lidasong", "123");
-        if (!$con) {
-            die('Could not connect: ' . mysql_error());
-        }
-        $db_selected = mysql_select_db('db_elwg_life', $con);
-        if (!$db_selected) {
-            die('Could not connect: ' . mysql_error());
-        }
-        $search = mysql_query("SELECT member_name FROM contacts");
-        if ($search == $name) {
+        $backArray[] = array();
+        $sql = "SELECT member_name FROM contacts WHERE member_name=$name";
+        $search = mysql_query($sql);
+        if ($search) {
             $result = mysql_query(" UPDATE contacts SET sex='$sex',grade='$grade',phone='$tel',
-		qq='$qq', email='$email' ");
+        qq='$qq', email='$email' ");
         } else $result = mysql_query("INSERT INTO contacts (member_name,sex,grade,phone,qq,email)
  VALUES ('$name','$sex','$grade','$tel','$qq','$email')");
-  if (!$result) {
+        if (!$result) {
             echo "no contacts information here in contacts";
             $message = 'Invalid query: ' . mysql_error() . "\n";
             die($message);
         }
+        mysql_close($con);
+    }
+    function search_contact_data($key) {
+        $sql = "SELECT member_name FROM contacts WHERE member_name LIKE '%$key%' ";
+        $search = mysql_query($sql);
+        $backArray = array();
+        while ($row = mysql_fetch_array($search)) {
+            $backArray[] = $row;
+        }
+        mysql_close($con);
+        return $backArray;
     }
 }
-?>            
+?>       
