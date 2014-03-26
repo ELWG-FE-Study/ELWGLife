d325b8992d893fa6fbdc1e6f9bf54aa020b5e6b8
lidasong <1584809032@qq.com>
2014-03-26 09:38:01 +0800

    models

--- models/contact_model.php
+++ models/contact_model.php
@@ -1,22 +1,25 @@
 <?php
 
 class Contact_model {
+
     function _construct() {
-        static $con = mysql_connect("115.156.216.95", "lidasong", "123");
+        static $con;
+        $con = mysql_connect("115.156.216.95" , "lidasong" , "123");
         if (!$con) {
             die('Could not connect: ' . mysql_error());
         }
-        static $db_selected = mysql_select_db('db_elwg_life', $con);
+        static $db_selected;
+        $db_selected = mysql_select_db('db_elwg_life', $con);
         if (!$db_selected) {
             die('Could not connect: ' . mysql_error());
         }
     }
+
     function get_contact_data（） {
         $sql = "SELECT * FROM contacts ";
         $result = mysql_query($sql);
         $backArray[] = array();
         if (!$result) {
-            echo "no contacts information here in contacts";
             $message = 'Invalid query: ' . mysql_error() . "\n";
             die($message);
         }
@@ -26,6 +29,7 @@ class Contact_model {
         mysql_close($con);
         return $backArray;
     }
+
     function update_contact_data($name, $sex, $grade, $tel, $qq, $email) {
         $backArray[] = array();
         $sql = "SELECT member_name FROM contacts WHERE member_name=$name";
@@ -36,12 +40,12 @@ class Contact_model {
         } else $result = mysql_query("INSERT INTO contacts (member_name,sex,grade,phone,qq,email)
  VALUES ('$name','$sex','$grade','$tel','$qq','$email')");
         if (!$result) {
-            echo "no contacts information here in contacts";
             $message = 'Invalid query: ' . mysql_error() . "\n";
             die($message);
         }
         mysql_close($con);
     }
+
     function search_contact_data($key) {
         $sql = "SELECT member_name FROM contacts WHERE member_name LIKE '%$key%' ";
         $search = mysql_query($sql);
