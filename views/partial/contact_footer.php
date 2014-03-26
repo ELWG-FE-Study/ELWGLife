f524436d433f7f07ce9887a97ff9cc9e0be125db
nihao <1032274844@qq.com>
2014-03-26 14:14:30 +0800

    修改header&footer部分代码

--- views/partial/contact_footer.php
+++ views/partial/contact_footer.php
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-=======
-<meta charset="utf-8">
-<link rel="stylesheet" type="text/css" href="../../css/contact.css">
-<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.4.2/pure-min.css"/>
-
->>>>>>> 6fd82cdd5550d15382033535597ff8eeafbf3783
 <div id="container">
 <div id="page" class="clearfix">
 </div>
@@ -13,14 +6,16 @@
 	<tbody>
 		<tr>
 			<td>
-			<p>
+			<p align="center">
 			湖北省智能互联网技术重点实验室（华中科技大学）&nbsp;&nbsp;
 			湖北省武汉市洪山区珞瑜路1037号 &nbsp;&nbsp;
 			邮 编：430074
-			<br>
+			</p>
+			<p align="center">
 			电话：027-87540745&nbsp;&nbsp;
 			传真：027-87540745&nbsp;&nbsp;
 			电子邮件：itec@hust.edu.cn
+			</p>
 			</td>
 		</tr>
 	</tbody>
