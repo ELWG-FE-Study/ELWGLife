07af8ce72a7b5e5b392f57042961790790a86e13
lidasong <1584809032@qq.com>
2014-03-29 16:53:54 +0800

    js

--- js/duty.js
+++ js/duty.js
@@ -1,24 +1,40 @@
-window.onload=function timer(){
-	var time = <?php echo date('H:i:s'); ?>
-	$('.headertime').innerHTML=time;
-	setTimeinterval('timer()',1000);
+window.onload = function(){
+	time=new Date();
+	week=time.getDay();
+	weekArray=["星期日","星期一","星期二","星期三","星期四","星期五","星期六"];
+	$(".weekday").innerHTML(weekArray[week]);
+	function timer(){	
+	var H=time.getHours();
+	var M=time.getMinutes();
+	var S=time.getSeconds();
+	M=checkTime(M);
+	S=checkTime(S);
+        $('.headertime').innerHTML = (H +':'+ M +':'+ S);
+    setTimeout('timer()', 1000);
+	}
 }
 
-$(document).ready(function(){
-	$('.nextweek').hide();
-	$('.nextweekButton').click(function(){
-	$('.nextweek').toggle();
-	});
-	$('.loginButton').click(function(){
-		$login=$('.login');
-		$.post('../page/duty_controller.php',{
-			username: $login.find('input[name=username]').val();
-			password: $login.find('input[name=password]').val();
-		},
-		function(){
-			if(data=='ok')    //登陆成功返回data，并且data=ok
-			$.post("../cookies.php?action=ok");
-		else return;
-		});
-	});
+function checkTime(t){
+if (t<10) 
+  t="0" + t;
+  return t;
+}
+
+$(document).ready(function() {
+    $('.nextweek').hide();
+    $('.nextweekButton').click(function() {
+        $('.nextweek').toggle();
+    });
+    $('.loginButton').click(function() {
+        $login = $('.login');
+        $.post('../page/duty_controller.php', {
+                username: $login.find('input[name=username]').val();
+                password: $login.find('input[name=password]').val();
+            },
+            function() {
+                if (data == 'ok') //登陆成功返回data，并且data=ok
+                    $.post("../cookies.php?action=ok");
+                else return;
+            });
+    });
 });
