2aab55c9b53beb52ba16c22d0b3abc74af5efaf0
lidasong <1584809032@qq.com>
2014-03-31 15:54:46 +0800

    views

--- views/duty_view.php
+++ views/duty_view.php
@@ -3,15 +3,15 @@
 <html>
 <head>
     <meta charset = "utf-8">
-    <?php include(ABS_PATH . 'dutyhead.php');?>
+    <?php include(ABS_PATH . 'views/duty/dutyhead.php');?>
 </head>
 <body>
 	<div class="pure-g-r">
     <div calss="pure-u" id="leftArea"></div>
     <div class="pure-u" id="centerArea"> 
-	<?php include(ABS_PATH . 'duty_header.php'); ?>
-	<?php include(ABS_PATH . 'duty_body.php'); ?>
-	<?php include(ABS_PATH . 'duty_footer.php'); ?>
+	<?php include(ABS_PATH . 'views/duty/duty_header.php'); ?>
+	<?php include(ABS_PATH . 'views/duty/duty_body.php'); ?>
+	<?php include(ABS_PATH . 'views/duty/duty_footer.php'); ?>
 </div>
 <div id="rightArea"></div>
 </body>
