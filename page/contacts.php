703a6d0fc3e52c8011f98f993ca94483f12c1be8
MrHowl <mrhowl@foxmail.com>
2014-04-01 21:56:29 +0800

    通讯录改进1.0
    1.微调了样式
    2.将添加条目与修改条目的按钮合并为一个
    3.添加条目或者修改条目后，及时刷新使通讯录完整（暂未用AJAX）
    待做：搜索，将通讯录用AJAX刷新，样式改动

--- page/contacts.php
+++ page/contacts.php
@@ -1,17 +1,7 @@
-<<<<<<< HEAD
 <?php 
 require_once('../base.php');
 error_reporting(E_ALL ^ E_NOTICE); 
 require('../controllers/contact_controller.php'); 
 $controller = new Contact_Controller(); 
 $controller->index(); 
-=======
-<?php 
-error_reporting(E_ERROR | E_WARNING | E_PARSE);
-require_once('../base.php');
-//error_reporting(E_All ^ E_NOTICE);
-require(ABS_PATH . 'controllers/contact_controller.php'); 
-$controller = new Contact_Controller(); 
-$controller->index(); 
->>>>>>> FETCH_HEAD
 /* End of file page/contacts.php */ 
--- views/partial/contact_content.php
+++ views/partial/contact_content.php
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 <div id="mainContent">
 <form class="pure-form">
     <input type = "text"  class="search_css" name = "key"/>
@@ -40,49 +35,3 @@
 <div id = "search_remind"></div>
 
 </div>
-=======
-=======
->>>>>>> FETCH_HEAD
-=======
->>>>>>> FETCH_HEAD
-=======
->>>>>>> FETCH_HEAD
-=======
->>>>>>> FETCH_HEAD
-<div>
-<form  class="pure-form" id="searchForm" action="../page/ajax/search_contact.php">
-    <input type = "text" class="search_css search_content" name = "s">
-    <button class="pure-button search_button">搜索</button>
-</form>
-<div id="result">
-    
-</div>
-
-
-<table class="pure-table ContactContent contact_table">
-    <thead>
-        <tr align="center">
-            <th width="10%">姓名</th>
-            <th width="10%">性别</th>
-            <th width="10%">年级</th>
-            <th width="20%">电话</th>
-            <th width="20%">QQ</th>
-            <th width="30%">常用邮箱</th>
-        </tr>
-    </thead>
-    <tbody>
-    <?php foreach ($data as $row) { ?>
-    <tr align="center">
-        <td ><?= $row["member_name"] ?></td>
-        <td ><?= $row["sex"] ?></td>
-        <td ><?= $row["grade"] ?></td>
-        <td ><?= $row["phone"] ?></td>
-        <td ><?= $row["qq"] ?></td>
-        <td ><?= $row["email"] ?></td>       
-    </tr>
-    <?php }?>
-    </tbody>
-</table>
-
-</div>
->>>>>>> FETCH_HEAD
