db4e009647f76c2d346049b6e48fb05d716bc8d3
nihao <1032274844@qq.com>
2014-04-01 16:57:03 +0800

    将duty_body中被写成静态的内容更改过来了。

--- views/duty/duty_body.php
+++ views/duty/duty_body.php
@@ -16,41 +16,25 @@
 				</tr>
 			</thead>
 			<tbody> <!-- $duty_result[]主要是model层返回的本周及次周值日成员 -->
-				<!--<tr>  
-					<td><?php $duty_result ?></td>
-					<td><?php $duty_result ?></td>
-					<td><?php $duty_result ?></td>
-					<td><?php $duty_result ?></td>	
-					<td><?php $duty_result ?></td>
-					<td><?php $duty_result ?></td>
-					<td><?php $duty_result ?></td>
-				</tr>
-				<tr class="nextweek">
-					<td><?php $duty_result ?></td>
-					<td><?php $duty_result ?></td>
-					<td><?php $duty_result ?></td>
-					<td><?php $duty_result ?></td>	
-					<td><?php $duty_result ?></td>
-					<td><?php $duty_result ?></td>
-					<td><?php $duty_result ?></td>
-				</tr>-->
-				<tr>
-					<td>王晨</td>
-					<td>王晨</td>
-					<td>王晨</td>
-					<td>王晨</td>
-					<td>王晨</td>
-					<td>王晨</td>
-					<td>王晨</td>
-				</tr>
-				<tr class="nextweek">
-					<td>王晨</td>
-					<td>王晨</td>
-					<td>王晨</td>
-					<td>王晨</td>
-					<td>王晨</td>
-					<td>王晨</td>
-					<td>王晨</td>
-				</tr>
+			<tbody> <!-- $duty_result[]主要是model层返回的本周及次周值日成员 -->
+            <tr>  
+                <td><?= $data[0]['member_name'] ?></td>
+                <td><?= $data[1]['member_name'] ?></td>
+                <td><?= $data[2]['member_name'] ?></td>
+                <td><?= $data[3]['member_name'] ?></td>   
+                <td><?= $data[4]['member_name'] ?></td>
+                <td><?= $data[5]['member_name'] ?></td>
+                <td><?= $data[6]['member_name'] ?></td>
+            </tr>
+            <tr class="nextweek">
+                <td><?= $data[7]['member_name'] ?></td>
+                <td><?= $data[8]['member_name'] ?></td>
+                <td><?= $data[9]['member_name'] ?></td>
+                <td><?= $data[10]['member_name'] ?></td>  
+                <td><?= $data[11]['member_name'] ?></td>
+                <td><?= $data[12]['member_name'] ?></td>
+                <td><?= $data[13]['member_name'] ?></td>
+            </tr>
+        </tbody>
 			</tbody>
 		</table>
