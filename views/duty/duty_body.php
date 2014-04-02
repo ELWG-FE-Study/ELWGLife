5010576bef5bbcde498055025af5f3ed228599f2
lidasong <1584809032@qq.com>
2014-04-02 20:13:11 +0800

    .

--- views/duty/duty_body.php
+++ views/duty/duty_body.php
@@ -1,56 +1,39 @@
-<!--<div id="dutytable"></div>-->
-	<p >值日表<button calss="pure-button" id="nextweekButton">下一周</button>
+<p >值日表<button calss="pure-button" id="nextweekButton">下一周</button>
+</p>
+<table class="pure-table pure-table-horizontal">			
+	<thead>
+		<tr>
+			<th>星期一</th>
+			<th>星期二</th>
+			<th>星期三</th>
+			<th>星期四</th>
+			<th>星期五</th>
+			<th>星期六</th>
+			<th>星期日</th>
+		</tr>
+	</thead>
+	<tbody> <!-- $data[]主要是model层返回的本周及次周值日成员 -->
+		<tr> 
+			<td><?php echo $result[0] ?></td>
+			<td><?php echo $result[1] ?></td>
+			<td><?php echo $result[2] ?></td>
+			<td><?php echo $result[3] ?></td>
+			<td><?php echo $result[4] ?></td>
+			<td><?php echo $result[5] ?></td>
+			<td><?php echo $result[6] ?></td>
+					
+					
+		</tr>
+		<tr class="nextweek">
+			<td><?php echo $result[7] ?></td>
+			<td><?php echo $result[8] ?></td>
+			<td><?php echo $result[9] ?></td>
+			<td><?php echo $result[10] ?></td>
+			<td><?php echo $result[11] ?></td>
+			<td><?php echo $result[12] ?></td>
+			<td><?php echo $result[13] ?></td>
+					
+		</tr>
+		</tbody>
+</table>
 
-	</p>
-
-		<table class="pure-table pure-table-horizontal">			
-			<thead>
-				<tr>
-					<th>星期一</th>
-					<th>星期二</th>
-					<th>星期三</th>
-					<th>星期四</th>
-					<th>星期五</th>
-					<th>星期六</th>
-					<th>星期日</th>
-				</tr>
-			</thead>
-			<tbody> <!-- $duty_result[]主要是model层返回的本周及次周值日成员 -->
-				<!--<tr>  
-					<td><?php $duty_result ?></td>
-					<td><?php $duty_result ?></td>
-					<td><?php $duty_result ?></td>
-					<td><?php $duty_result ?></td>	
-					<td><?php $duty_result ?></td>
-					<td><?php $duty_result ?></td>
-					<td><?php $duty_result ?></td>
-				</tr>
-				<tr class="nextweek">
-					<td><?php $duty_result ?></td>
-					<td><?php $duty_result ?></td>
-					<td><?php $duty_result ?></td>
-					<td><?php $duty_result ?></td>	
-					<td><?php $duty_result ?></td>
-					<td><?php $duty_result ?></td>
-					<td><?php $duty_result ?></td>
-				</tr>-->
-				<tr>
-					<td>王晨</td>
-					<td>王晨</td>
-					<td>王晨</td>
-					<td>王晨</td>
-					<td>王晨</td>
-					<td>王晨</td>
-					<td>王晨</td>
-				</tr>
-				<tr class="nextweek">
-					<td>王晨</td>
-					<td>王晨</td>
-					<td>王晨</td>
-					<td>王晨</td>
-					<td>王晨</td>
-					<td>王晨</td>
-					<td>王晨</td>
-				</tr>
-			</tbody>
-		</table>
