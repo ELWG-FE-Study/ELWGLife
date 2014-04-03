dbade62999cb5dd910cb50a2d212b6b69fe48dc0
lidasong <1584809032@qq.com>
2014-04-03 10:16:12 +0800

    .

--- page/duty.php
+++ page/duty.php
@@ -1,9 +1,6 @@
 <?php
-<<<<<<< HEAD
-error_reporting(E_ALL ^ E_NOTICE); 
-=======
+
 error_reporting(E_ERROR | E_WARNING | E_PARSE);
->>>>>>> 9ec8b7abee4742d92222d3b5cc15b34367da803e
 require_once('../base.php');
 require(ABS_PATH . 'controllers/duty_controller.php'); 
 $controller = new Duty_Controller(); 
