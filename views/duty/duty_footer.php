81c9974b6419ba26e50d147ddcca53706c20dcc9
lidasong <1584809032@qq.com>
2014-04-04 09:44:52 +0800

    duty_foooter添加登录

--- views/duty/duty_footer.php
+++ views/duty/duty_footer.php
@@ -8,13 +8,14 @@
 		<li>拖地</li>
 	</ol>
 </div>
-<div class="pure-form login">
+<div class="pure-form"id="loginCheck">
 	<input type="text" name="username" placeholder="用户名" />
 	<br/>
 	<br/>
 	<input type="password" name="password" placeholder="密码" />
 	<br/>
 	<br/>
-	<button type="button" class="pure-button pure-button-primary loginButton " >总管登录</button>
+	<button type="button" class="pure-button pure-button-primary loginButton">
+		总管登录</button>
 
 </div>
